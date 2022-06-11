0fd8a13052b463a6a1e8e5e9678965cd4e822f61
Nhut Nguyen <ntnhut17@gmail.com>
2022-06-11 21:28:39 +0200

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/mergeSortedList.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    if (list1 == nullptr) {
+        return list2;
+    } else if (list2 == nullptr) {
+        return list1;
+    }
+    // identify which list is head
+    ListNode* head = list1;
+    if (list2->val < head->val) {
+        head = list2;
+        list2 = list2->next;
+    } else {
+        list1 = list1->next;
+    }
+    ListNode* node = head;
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            node->next = list1;
+            list1 = list1->next;
+        } else {
+            node->next = list2;
+            list2 = list2->next;
+        }
+        node = node->next;
+    }
+    if (list1 == nullptr) {
+        node->next = list2;
+    } else {
+        node->next = list1;
+    }
+    return head;
+}
+
+void printResult(ListNode* head) {
+    std::cout << "[";
+    while (head) {
+        std::cout << head->val << ",";
+        head = head->next;
+    }
+    std::cout << "]\n";
+}
+int main() {   
+    ListNode four1(4);
+    ListNode two1(2, &four1);
+    ListNode one1(1, &two1);
+    ListNode four2(4);
+    ListNode three2(3, &four2);
+    ListNode one2(1, &three2);
+    auto newOne = mergeTwoLists(&one1, &one2);
+    printResult(newOne); // [1,1,2,3,4,4,]
+
+    auto empty = mergeTwoLists(nullptr, nullptr);
+    printResult(empty); // []
+
+    ListNode zero(0);
+    auto z = mergeTwoLists(nullptr, &zero);
+    printResult(z); // [0,]
+}
