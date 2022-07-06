23719a8d4e94543a31898c0836d640319f5c8bd1
Nhut Nguyen <ntnhut17@gmail.com>
2022-07-06 21:01:02 +0200

    509. Fibonacci Number

---
+++ 509. Fibonacci Number/509_Fibonacci_Number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int f0 = 0;
+    int f1 = 1;
+    for (int i = 2; i <= n; i++) {
+        int f2 = f1 + f0;
+        f0 = f1;
+        f1 = f2;
+    }
+    return f1;
+}
+
+
+int main() {
+    std::cout << fib(2) << std::endl;
+    std::cout << fib(3) << std::endl;
+    std::cout << fib(4) << std::endl;
+}
