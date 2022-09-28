e5e1f6fa66905a50cfb11b8a4c85b93659cf7b6c
Nhut Nguyen <ntnhut17@gmail.com>
2022-09-28 21:50:22 +0200

    19. Remove Nth Node From End of List

---
+++ 19. Remove Nth Node From End of List/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+using namespace std;
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* fast = head;
+    for (int i = 0; i < n; i++) {
+        fast = fast->next;
+    }
+    if (fast == nullptr) {
+        return head->next;
+    }
+    ListNode* slow = head;
+    while (fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    slow->next = slow->next->next;
+    return head;
+}
+void printList(ListNode *head) {
+    ListNode* node = head;
+    cout << "[";
+    while (node) {
+        cout << node->val << ",";
+        node = node->next;
+    }
+    cout << "]\n";
+}
+int main() {
+    ListNode five(5);
+    ListNode four(4, &five);
+    ListNode three(3, &four);
+    ListNode two(2, &three);
+    ListNode one(1, &two);
+    auto head = removeNthFromEnd(&one, 2);
+    printList(head);
+    head = removeNthFromEnd(&five, 1);
+    printList(head);
+    head = removeNthFromEnd(&four, 1);
+    printList(head);
+}
