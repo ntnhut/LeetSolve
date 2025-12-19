6f9563631f08b34735b50c00cd34b23896a1454f
Nhut Nguyen <ntnhut17@gmail.com>
2025-12-19 10:46:38 +0100

    2166 design-bitset

---
+++ code/2166_design-bitset.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <vector>
+#include <print>
+#include <cassert>
+using namespace std;
+class Bitset {
+    string bits;
+    int count_;
+public:
+    Bitset(int size) : bits(size, '0'), count_(0) {        
+    }
+    
+    void fix(int idx) {
+        if (bits[idx] == '0') {
+            bits[idx] = '1';
+            count_++;
+        }        
+    }
+    
+    void unfix(int idx) {
+        if (bits[idx] == '1') {
+            bits[idx] = '0';
+            count_--;
+        }
+    }
+    
+    void flip() {
+        for (auto& c : bits) {
+            if (c == '1') {
+                c = '0';
+            }
+            else {
+                c = '1';
+            }
+        }
+        count_ = bits.size() - count_;
+    }
+    
+    bool all() {
+        return count_ == bits.size();
+    }
+    
+    bool one() {
+        return count_ >= 1;
+    }
+    
+    int count() {
+        return count_;
+    }
+    
+    string& toString() {
+        return bits;
+    }
+};
+
+int main() {
+    Bitset bs(5);
+    assert(bs.toString() == "00000");
+    bs.fix(3);
+    bs.fix(1);
+    bs.flip();
+    assert(bs.all() == false);
+    bs.unfix(0);
+    bs.flip();
+    assert(bs.one() == true);
+    bs.unfix(0);
+    assert(bs.count() == 2);
+    assert(bs.toString() == "01010");
+    return 0;
+}
